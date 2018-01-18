406725377df858408bd3560620977fe283347a27
Administrator <790168710@qq.com>
2018-01-18 18:42:33 +0800

    首页基础排版

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="content" v-if="homeData.data">
     <swiper :banners="homeData.data.banner"></swiper>
+    <div class="wrap">
+      <div class="wrap-left">
+        <div class="l-top">
+          <img src="../assets/home_03.png"/>
+          <p class="t-name">美科星学院</p>
+        </div>
+        <div class="l-bottom"></div>
+      </div>
+      <div class="wrap-right">
+        <div class="r-top"></div>
+        <div class="r-bottom"></div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -43,8 +56,52 @@
 <style scoped lang="less">
   .content {
     width: 100%;
-    img {
-      width: 100%;
+    .wrap {
+      width: 1200px;
+      margin: 40px auto 0 auto;
+      padding-bottom: 100px;
+      height: 794px;
+      .wrap-left {
+        float: left;
+        width: 700px;
+        .l-top {
+          width: 100%;
+          position: relative;
+          margin-bottom: 26px;
+          img {
+            width: 100%;
+          }
+          .t-name{
+            position: absolute;
+            left: 30px;
+            top:26px;
+            font-size: 24px;
+            color: #fff;
+          }
+        }
+        .l-bottom{
+          height: 366px;
+          width: 100%;
+          background: gray;
+          margin-bottom: 26px;
+        }
+      }
+      .wrap-right{
+        float: right;
+        width: 474px;
+        .r-top{
+          width: 100%;
+          background: blue;
+          height: 247px;
+          margin-bottom: 26px;
+        }
+        .r-bottom{
+          width: 100%;
+          background: yellowgreen;
+          height: 437px;
+          margin-bottom: 26px;
+        }
+      }
     }
   }
 </style>
