cde62911bcdf26707192b07f9c6f3150bd6e3a42
Administrator <790168710@qq.com>
2018-01-20 13:26:55 +0800

    展馆概况

--- src/components/exhibit/survey.vue
+++ src/components/exhibit/survey.vue
@@ -4,6 +4,15 @@
       :navs="navs"
       :banner="banner"
       :title="title"/>
+    <div class="item">
+      <p class="title">全新科技馆<span></span></p>
+      <div class="text-info">
+        <p class="txt">
+          四川科技馆位于成都市中心天府广场北侧，处于成都市中央商务区中的文化博览区地标位置。占地面积60000平方米，建筑面积41800平方米。<br/>
+          四川科技馆由原四川省展览馆改建而成，于2006年11月2日正式建成开馆，是一座多学科、多角度、全方位大型综合科普场馆，馆内常设展场面积2.5万平方米。经过近10年运行，于2016年6月18日正式闭馆改造。
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 <script type="text/ecmascript-6">
@@ -51,5 +60,36 @@
   }
 </script>
 <style lang="less">
-
+  .survey-con{
+    .item{
+      margin-top: 50px;
+      .title{
+        font-size: 20px;
+        color: #666;
+        text-align: center;
+        margin-bottom: 25px;
+        span{
+          width:80px;
+          height: 4px;
+          display: block;
+          margin: 10px auto;
+          background-image: -webkit-linear-gradient(135deg, #00d0fb, #00acf6);
+          background-image: -moz-linear-gradient(135deg, #00d0fb, #00acf6);
+          background-image: -o-linear-gradient(135deg, #00d0fb, #00acf6);
+          background-image: -ms-linear-gradient(135deg, #00d0fb, #00acf6);
+          background-image: linear-gradient(135deg, #00d0fb, #00acf6);
+          -webkit-border-radius: 10px;
+          -moz-border-radius: 10px;
+          border-radius: 10px;
+        }
+      }
+      .text-info{
+        text-align: center;
+        .txt{
+          font-size: 14px;
+          line-height: 25px;
+        }
+      }
+    }
+  }
 </style>
