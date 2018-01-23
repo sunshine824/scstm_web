d13be68af50a7ca49cde6498a9340dd23a0ae19c
Administrator <790168710@qq.com>
2018-01-23 15:48:00 +0800

    参观咨询

---
+++ src/base/visit_serve/consult.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="consult-txt">
+    <div class="item clearfix">
+      <div class="title">四川科技馆2018年春节期间开闭馆公告</div>
+      <div class="txt">
+        <p>2018年春节即将来临，根据国家有关春节放假安排的通知，结合我馆实际情况，现将我馆2018年春节期间开、闭馆时间通知如下：</p>
+        <p>2月13日（腊月二十八）至2月16日（正月初一）闭馆；</p>
+        <p>2月17日（初 二）至2月21日（初 六）开馆；</p>
+        <p>2月22日（初七）、23日（初八）闭馆；</p>
+        <p>2月24日起正常开闭馆。</p>
+        <p>★为满足公众节日期间参观需求，结合我馆内实际运行情况，2月17日至2月21日期间，在每日5000个预约名额的基础上，增加1000个预约名额。</p>
+        <p>★请公众根据预约情况合理安排出行。科技馆实行网上预约参观，现场没有免费券领取。</p>
+        <p>1F - 3F展区：周一至周日09:00——17:00 【16:00停止领券入馆】(一人一券凭身份证领券入馆)</p>
+        <p>4F未来学院：周一至周日09:00——20:30 【出入口在大楼北面】</p>
+        <p>周一闭馆，法定节假日开闭馆时间另行公告，见官网http://www.scstm.com。</p>
+      </div>
+    </div>
+    <div class="item clearfix">
+      <div class="title">参观方式</div>
+      <div class="txt">
+        <p>1、凭本人身份证通过四川科技馆网站或微信公众号,提前一周进行参观预约。一人一券，凭券入馆，参观券当日一次有效。</p>
+        <p>按照预约时间携带【本人身份证】，到四川科技馆服务大厅自助取票机凭【本人身份证】免费换券入馆。</p>
+        <p>3、严禁携带易燃易爆物品，有毒有害物品，管制刀具及宠物入馆，馆内严禁明火、吸烟。</p>
+        <p>4、身高1.4米以下儿童、高龄老人及行动不便者须在亲友的陪同下入馆参观。</p>
+        <p>5、1位成人可带1位儿童入馆参观，随行儿童也需录入身份证信息，凭预约取票人身份证领取参观券入馆参观。</p>
+        <p>6、预约成功后，如无法参观，请取消订单。超过2次（含2次）未取消订单者，将无法参观预约。</p>
+        <p>7、请公众根据预约情况，合理安排出行。科技馆实行网上预约参观，现场没有免费券领取。</p>
+      </div>
+    </div>
+    <div class="item clearfix">
+      <div class="title">联系我们</div>
+      <div class="txt">
+        <p>四川科技馆网址：http://www.scstm.com</p>
+        <p>咨询电话：028-86609999</p>
+        <p>微信公众号：四川科技馆</p>
+      </div>
+    </div>
+  </div>
+</template>
+<script type="text/ecmascript-6">
+  export default {
+    data() {
+      return {}
+    }
+  }
+</script>
+<style lang="less">
+  .consult-txt{
+    width: 100%;
+    padding-bottom: 60px;
+    .item{
+      width: 100%;
+      margin-bottom: 15px;
+      .title{
+        font-size: 28px;
+        color: #333;
+        padding-bottom: 30px;
+        border-bottom:1px solid #dcdcdc;
+        margin-top: 30px;
+      }
+      .txt{
+        color: #333;
+        margin-top: 10px;
+        p{
+          padding-top: 15px;
+          font-size: 16px;
+        }
+      }
+    }
+  }
+</style>
--- src/components/visit_serve/consult.vue
+++ src/components/visit_serve/consult.vue
@@ -1,3 +1,95 @@
 <template>
-  <div>参观咨询</div>
+  <div class="consult">
+    <banner
+      :navs="navs"
+      :banner="banner"
+      :title="title"
+      @handleClick="getBanner"/>
+    <bg>
+      <div class="consult-con">
+        <nav-bar :navBar="navBar"
+                 @handleClick="handleTypeClick">
+        </nav-bar>
+        <consult v-if="typeId===1"></consult>
+      </div>
+    </bg>
+  </div>
 </template>
+<script type="text/ecmascript-6">
+  import Banner from '@/base/banner'
+  import {getBannerMixin} from '@/public/js/mixin'
+  import NavBar from '@/base/navBar'
+  import Bg from '@/base/bg'
+  import Consult from '@/base/visit_serve/consult'
+
+  export default {
+    mixins: [getBannerMixin],
+    components: {
+      NavBar,
+      Banner,
+      Bg,
+      Consult
+    },
+    data() {
+      return {
+        navBar: [
+          {title: '个人预约', id: 1},
+          {title: '团队预约', id: 2, href: 'https://www.baidu.com'},
+          {title: '个人预约查询', id: 3, href: 'https://www.baidu.com'},
+          {title: '团队预约查询', id: 4, href: 'https://www.baidu.com'},
+        ],
+        navs: [
+          {
+            href: '/visit_serve/strategy',
+            title: '参观攻略',
+            id: 1
+          },
+          {
+            href: '/visit_serve/act_calendar',
+            title: '活动日历',
+            id: 2
+          },
+          {
+            href: '/visit_serve/SE',
+            title: 'SE餐厅',
+            id: 3
+          },
+          {
+            href: '/visit_serve/consult',
+            title: '参观咨询',
+            id: 4
+          },
+          {
+            href: '/visit_serve/notice',
+            title: '新闻公告',
+            id: 5
+          }
+        ],
+        title: '参观服务',
+        typeId: 1
+      }
+    },
+    created() {
+      this.getBanner()
+    },
+    methods: {
+      /**
+       * 获取参观咨询banner
+       * @param id  分类id
+       */
+      getBanner() {
+        this.getBannerData({id: '', url: 'api/consulbanner'})
+      },
+      handleTypeClick(typeId) {
+        this.typeId = typeId
+      },
+    }
+  }
+</script>
+<style lang="less">
+  .consult-con {
+    width: 1200px;
+    margin: 0 auto;
+    padding-top: 50px;
+  }
+</style>
